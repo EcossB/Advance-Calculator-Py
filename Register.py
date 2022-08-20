b71c5ba8781c5e4fc6e33e74dad3b6521d1bab35
EcossB <erwingcoss21@hotmail.com>
2022-08-20 16:01:49 -0400

    interfaz para agregar personajes de anime

--- Register.py
+++ Register.py
@@ -8,8 +8,54 @@ class anime_interface:
         self.window.title('Gestionar personajes')
         
         #creacion del contenedor
-        frame1 = LabelFrame(self.window,text= 'Add a new character')
-        frame1.grid(row = 0, column= 0, columnspan=3, pady= 20)
+        frame1 = LabelFrame(self.window,text= 'Add a new character', )
+        frame1.grid(row = 0, column= 0, columnspan=6, pady= 20)
+
+        #data inputs
+        #input for the name
+        Label(frame1, text= "Name: ").grid(row= 1, column=0)
+        self.name = Entry(frame1)
+        self.name.focus()
+        self.name.grid(row= 1, column=1)
+
+
+        #for the last name
+        Label(frame1, text= "LastName: ").grid(row= 2, column=0)
+        self.last_name = Entry(frame1).grid(row= 2, column=1)
+
+        #for the photo
+        Label(frame1, text= "URL_Photo: ").grid(row= 3, column=0)
+        self.photo = Entry(frame1).grid(row= 3, column=1)
+
+        #Serie
+        Label(frame1, text= "LastName: ").grid(row= 4, column=0)
+        self.photo = Entry(frame1).grid(row= 4, column=1)
+
+        #Birth 
+        Label(frame1, text= "Birth Date: ").grid(row= 5, column=0)
+        self.birth = Entry(frame1).grid(row= 5, column=1)
+
+        #button add characters
+        ttk.Button(frame1, text="Save character").grid(row= 6, columnspan=2, sticky= W + E)
+
+        #creating a table to view the data 
+        columns = ('name', 'last name', 'url photo', 'datebirth')
+
+        self.table = ttk.Treeview(height =10, columns= columns)
+        self.table.grid(row= 7, column= 0, columnspan=  5)
+
+        #creating the headings of the data
+        self.table.heading('#0', text = 'Name', anchor= CENTER)
+        self.table.heading('#1', text = 'Last Name', anchor= CENTER)
+        self.table.heading('#2', text = 'Url photo', anchor= CENTER)
+        self.table.heading('#3', text = 'Series', anchor= CENTER)
+        self.table.heading('#4', text = 'Date birth', anchor= CENTER)
+
+
+
+
+
+
 
 if __name__=='__main__':
     root = Tk()
