3cc960b734eed9e303dad9fefca5044d8f1043db
EcossB <erwingcoss21@hotmail.com>
2022-08-21 23:46:14 -0400

    First crud complete

--- Register.py
+++ Register.py
@@ -1,10 +1,8 @@
-from ast import Raise
-from cProfile import label
-from optparse import Values
 from tkinter import * 
 from tkinter import ttk
 import sqlite3
-from tokenize import String
+
+back = "#add8e6"
 
 class anime_interface:
     database = 'Anime_prog1.db'
@@ -12,6 +10,7 @@ class anime_interface:
     def __init__(self, root):
         self.window = root
         self.window.title('Gestionar personajes')
+        
         
         #creacion del contenedor
         frame1 = LabelFrame(self.window,text= 'Add a new character', )
@@ -97,7 +96,8 @@ class anime_interface:
         #creating a table to view the data 
         columns = ('id','name', 'last name', 'url photo', 'datebirth', 'p','ph','a','s','st','ad','lat','lon')
 
-        self.table = ttk.Treeview(height =10, columns= columns)
+        self.table = ttk.Treeview(height =10, columns= columns )
+        self.table.tag_configure('oddrow', background= back)
         self.table.grid(row= 16, column= 0, columnspan=  5)
 
         #creating the headings of the data
@@ -157,7 +157,7 @@ class anime_interface:
 
         #and here we are filling the table with the data
         for row in data_row:
-            self.table.insert('',0, text = row[0], values =(row[1], row[2], row[3],row[4],row[5],row[6],row[7],row[8],row[9],row[10],row[11],row[12],row[13]))
+            self.table.insert('',0, text = row[0], values =(row[1], row[2], row[3],row[4],row[5],row[6],row[7],row[8],row[9],row[10],row[11],row[12],row[13]), tags= 'oddrow', )
     
     def validate(self):
        return len(self.name.get()) != 0 and len(self.last_name.get()) != 0 #and len(self.serie.get()) != 0 and len(self.sex.get()) != 0 and len(self.state.get()) != 0
@@ -213,45 +213,140 @@ class anime_interface:
         except IndexError as e:
             self.ms['text'] = 'Please select a row'
             return 
-        name = self.table.item(self.table.selection())['values'][0]
-        lastname = self.table.item(self.table.selection())['values'][1]
-        url = self.table.item(self.table.selection())['values'][2]
-        ser = self.table.item(self.table.selection())['values'][3]
-        bir = self.table.item(self.table.selection())['values'][4]
-        power = self.table.item(self.table.selection())['values'][5]
-        phr = self.table.item(self.table.selection())['values'][6]
-        age = self.table.item(self.table.selection())['values'][7]
-        sex = self.table.item(self.table.selection())['values'][8]
-        state = self.table.item(self.table.selection())['values'][9]
-        ad = self.table.item(self.table.selection())['values'][10]
+        #these are the old data that will be updated
+        o_name = self.table.item(self.table.selection())['values'][0]
+        o_lastname = self.table.item(self.table.selection())['values'][1]
+        ourl = self.table.item(self.table.selection())['values'][2]
+        oser = self.table.item(self.table.selection())['values'][3]
+        obir = self.table.item(self.table.selection())['values'][4]
+        opower = self.table.item(self.table.selection())['values'][5]
+        ophr = self.table.item(self.table.selection())['values'][6]
+        oage = self.table.item(self.table.selection())['values'][7]
+        osex = self.table.item(self.table.selection())['values'][8]
+        ostate = self.table.item(self.table.selection())['values'][9]
+        oad = self.table.item(self.table.selection())['values'][10]
+        olat = self.table.item(self.table.selection())['values'][11]
+        ole = self.table.item(self.table.selection())['values'][12]
 
         self.edit_wind = Toplevel()
-        self.edit_wind.title = "Edit character"
+        self.edit_wind.title = 'Edit character'
 
         #old name
-        Label(self.edit_wind, text = "Old name: ").grid(row= 0, column=1)
-        Entry(self.edit_wind, textvariable= StringVar(self.edit_wind, value= name), state = 'readonly').grid(row = 0, column= 2)
+        Label(self.edit_wind, text = "Old name: ", width= 15).grid(row= 0, column=1)
+        Entry(self.edit_wind, textvariable= StringVar(self.edit_wind, value= o_name), state = 'readonly', width= 30).grid(row = 0, column= 2)
         #new name
-        Label(self.edit_wind, text = "New name: ").grid(row= 1, column=1)
-        self.new_name = Entry(self.edit_wind).grid(row= 1, column = 2)
+        Label(self.edit_wind, text = "New name: ", ).grid(row= 1, column=1)
+        new_name = Entry(self.edit_wind)
+        new_name.grid(row= 1, column = 2)
 
         #Old last name
         ttk.Label(self.edit_wind, text = "Old Last Name: ").grid(row= 2, column=1)
-        Entry(self.edit_wind, textvariable= StringVar(self.edit_wind, value= lastname), state = 'readonly').grid(row = 2, column= 2)
+        Entry(self.edit_wind, textvariable= StringVar(self.edit_wind, value= o_lastname), state = 'readonly', width= 30).grid(row = 2, column= 2)
         #new last_name
         Label(self.edit_wind, text = "New Last name: ").grid(row= 3, column=1)
-        self.new_last = Entry(self.edit_wind).grid(row= 3, column = 2)
+        new_last = Entry(self.edit_wind)
+        new_last.grid(row= 3, column = 2)
     
         #Old url
         Label(self.edit_wind, text = "Old URL: ").grid(row= 4, column=1)
-        Entry(self.edit_wind, textvariable= StringVar(self.edit_wind, value= url), state = 'readonly').grid(row = 4, column= 2)
+        Entry(self.edit_wind, textvariable= StringVar(self.edit_wind, value= ourl), state = 'readonly', width= 30).grid(row = 4, column= 2)
         #New url
         Label(self.edit_wind, text = "New URL: ").grid(row= 5, column=1)
-        self.new_url = Entry(self.edit_wind).grid(row= 5, column = 2)
+        new_url = Entry(self.edit_wind)
+        new_url.grid(row= 5, column = 2)
 
+        #Old data (series, sex, etcetera)
+        Label(self.edit_wind, text = "Old Series: ").grid(row= 6, column=1)
+        Entry(self.edit_wind, textvariable= StringVar(self.edit_wind, value= oser), state = 'readonly', width= 30, bg= 'white').grid(row = 6, column= 2)
+
+        Label(self.edit_wind, text = "Old Date: ").grid(row= 8, column=1)
+        Entry(self.edit_wind, textvariable= StringVar(self.edit_wind, value= obir), state = 'readonly', width= 30).grid(row = 8, column= 2)
+
+        Label(self.edit_wind, text = "Old Power: ").grid(row= 10, column=1)
+        Entry(self.edit_wind, textvariable= StringVar(self.edit_wind, value= opower), state = 'readonly', width= 30).grid(row = 10, column= 2)
+
+        Label(self.edit_wind, text = "Old Phrase: ").grid(row= 12, column=1)
+        Entry(self.edit_wind, textvariable= StringVar(self.edit_wind, value= ophr), state = 'readonly', width= 30).grid(row = 12, column= 2)
+
+        Label(self.edit_wind, text = "Old Age: ").grid(row= 14, column=1)
+        Entry(self.edit_wind, textvariable= StringVar(self.edit_wind, value= oage), state = 'readonly', width= 30).grid(row = 14, column= 2)
+
+        Label(self.edit_wind, text = "Old Sex: ").grid(row= 16, column=1)
+        Entry(self.edit_wind, textvariable= StringVar(self.edit_wind, value= osex), state = 'readonly', width= 30).grid(row = 16, column= 2)
+
+        Label(self.edit_wind, text = "Old State: ").grid(row= 18, column=1)
+        Entry(self.edit_wind, textvariable= StringVar(self.edit_wind, value= ostate), state = 'readonly', width= 30).grid(row = 18, column= 2)
+
+        Label(self.edit_wind, text = "Old Adress: ").grid(row= 20, column=1)
+        Entry(self.edit_wind, textvariable= StringVar(self.edit_wind, value= oad), state = 'readonly', width= 30).grid(row = 20, column= 2)
         
+        Label(self.edit_wind, text = "Old Latitude: ").grid(row= 22, column=1)
+        Entry(self.edit_wind, textvariable= StringVar(self.edit_wind, value= olat), state = 'readonly', width= 30).grid(row = 22, column= 2)
+       
+        Label(self.edit_wind, text = "Old Lenght: ").grid(row= 24, column=1)
+        Entry(self.edit_wind, textvariable= StringVar(self.edit_wind, value= ole), state = 'readonly', width= 30).grid(row = 24, column= 2)
+
+        #New all data (series, sex, etc)
+        Label(self.edit_wind, text = "New Series: ").grid(row= 7, column=1)
+        new_serie = Entry(self.edit_wind)
+        new_serie.grid(row= 7, column = 2)
+
+        Label(self.edit_wind, text = "New Date: ").grid(row= 9, column=1)
+        new_date = Entry(self.edit_wind)
+        new_date.grid(row= 9, column = 2)
+
+        Label(self.edit_wind, text = "New Power: ").grid(row= 11, column=1)
+        new_power = Entry(self.edit_wind)
+        new_power.grid(row= 11, column = 2)
+
+        Label(self.edit_wind, text = "New Phrase: ").grid(row= 13, column=1)
+        new_phrase = Entry(self.edit_wind)
+        new_phrase.grid(row= 13, column = 2)
+
+        Label(self.edit_wind, text = "New Age: ").grid(row= 15, column=1)
+        new_age = Entry(self.edit_wind)
+        new_age.grid(row= 15, column = 2)
+
+        Label(self.edit_wind, text = "New Sex: ").grid(row= 17, column=1)
+        new_sex = Entry(self.edit_wind)
+        new_sex.grid(row= 17, column = 2)
+
+        Label(self.edit_wind, text = "New State: ").grid(row= 19, column=1)
+        new_state = Entry(self.edit_wind)
+        new_state.grid(row= 19, column = 2)
+
+        Label(self.edit_wind, text = "New Adress: ").grid(row= 21, column=1)
+        new_adress = Entry(self.edit_wind)
+        new_adress.grid(row= 21, column = 2)
+
+        Label(self.edit_wind, text = "New latitude: ").grid(row= 23, column=1)
+        new_latitude = Entry(self.edit_wind)
+        new_latitude.grid(row= 23, column = 2)
+
+        Label(self.edit_wind, text = "New Lenght: ").grid(row= 25, column=1)
+        new_lenght = Entry(self.edit_wind)
+        new_lenght.grid(row= 25, column = 2)
+
+        Button(self.edit_wind, text = 'Update data character', command=lambda: self.edit_row(new_name.get(), o_name, new_last.get(), o_lastname, new_url.get(), ourl, new_serie.get(), oser, new_date.get(), obir, new_power.get(), opower, new_phrase.get(), ophr, new_age.get(), oage, new_sex.get(), osex, new_state.get(), ostate, new_adress.get(), oad, new_latitude.get(), olat, new_lenght.get(), ole)).grid(row = 26, column = 2, sticky= W)
+
+    def edit_row(self, new_name, name, new_last, lastname, new_url, url, new_series, 
+    ser, new_date, bir, new_power, power, new_phrase ,
+    phr, new_age , age,new_sex , sex,new_state , state,new_adress,
+    ad ,new_latitude ,lat, new_lenght ,le):
+        query = 'UPDATE personajess SET name = ?, last_name = ?, photo = ?, serie = ?, fecha_nacimiento = ?, Poder = ?, frase_favorita = ?, edad = ?, sexos = ?, estados = ?, direccion = ?, Latitud = ?, Field14 = ? WHERE name = ? AND last_name= ? AND photo = ? AND serie =? AND fecha_nacimiento = ? AND Poder = ? AND frase_favorita = ? AND edad = ? AND sexos = ? AND estados = ? AND direccion = ? AND Latitud = ? AND Field14 = ?'
+        Parameter = (new_name, new_last, new_url, new_series, new_date, new_power, new_phrase, new_age, new_sex, new_state, new_adress, new_latitude, new_lenght, name, lastname, url, ser, bir, power, phr, age, sex, state, ad, lat, le)
+        self.get_query(query, Parameter)
+        self.edit_wind.destroy()
+        self.ms['text'] = 'record {} {} updated successfuly'.format(name, lastname)
+        self.get_data()
+
+
+
+
 
 if __name__=='__main__':
     root = Tk()
     open_app = anime_interface(root)  
+    root.configure(background= back)
     root.mainloop()
+    
