5efc90532c63e742a60eb331781ee247cf088463
EcossB <erwingcoss21@hotmail.com>
2022-08-25 23:34:52 -0400

    creacion de diferentes metodos para ingresar los numeros y operacions, tambien para calcular la operacion puesta por el usuario

--- calculator.py
+++ calculator.py
@@ -1,7 +1,7 @@
-from ast import Global
-import re
+from ast import Expression
 from tkinter import *
 from math import *
+import parser
 
 i = 0
 #creatio of the class 
@@ -30,28 +30,59 @@ class calculator:
         Button(self.wind, text="8", width= 9, height= 3, command=lambda:get_numbers(8)).grid(row=3, column=1, sticky= W+E)
         Button(self.wind, text="9", width= 9, height= 3, command=lambda:get_numbers(9)).grid(row=3, column=2, sticky= W+E)
 
-        Button(self.wind, text="C", width= 9, height= 3).grid(row=4, column=0, sticky= W+E)
-        Button(self.wind, text="0", width= 9, height= 3).grid(row=4, column=1, sticky= W+E)
-        Button(self.wind, text="%", width= 9, height= 3).grid(row=4, column=2, sticky= W+E)
+        Button(self.wind, text="C", width= 9, height= 3, command=lambda:clear()).grid(row=4, column=0, sticky= W+E)
+        Button(self.wind, text="0", width= 9, height= 3, command=lambda:get_numbers(0)).grid(row=4, column=1, sticky= W+E)
+        Button(self.wind, text="%", width= 9, height= 3, command=lambda:get_operations("%")).grid(row=4, column=2, sticky= W+E)
         
         #operations buttons
-        Button(self.wind, text="+", width= 9, height= 3).grid(row=1, column=3, sticky= W+E)
-        Button(self.wind, text="-", width= 9, height= 3).grid(row=2, column=3, sticky= W+E)
-        Button(self.wind, text="*", width= 9, height= 3).grid(row=3, column=3, sticky= W+E)
-        Button(self.wind, text="/", width= 9, height= 3).grid(row=4, column=3, sticky= W+E)
+        Button(self.wind, text="+", width= 9, height= 3, command=lambda:get_operations("+")).grid(row=1, column=3, sticky= W+E)
+        Button(self.wind, text="-", width= 9, height= 3, command=lambda:get_operations("-")).grid(row=2, column=3, sticky= W+E)
+        Button(self.wind, text="x", width= 9, height= 3, command=lambda:get_operations("*")).grid(row=3, column=3, sticky= W+E)
+        Button(self.wind, text=u"\u00f7", width= 9, height= 3, command=lambda:get_operations("/")).grid(row=4, column=3, sticky= W+E)
 
-        Button(self.wind, text=u"\u232B", width= 9, height= 3).grid(row=1, column=4, sticky= W+E, columnspan= 2)
-        Button(self.wind, text="EXP", width= 9, height= 3).grid(row=2, column=4, sticky= W+E)
-        Button(self.wind, text="^2", width= 9, height= 3).grid(row=2, column=5, sticky= W+E)
-        Button(self.wind, text="(", width= 9, height= 3).grid(row=3, column=4, sticky= W+E)
-        Button(self.wind, text=")", width= 9, height= 3).grid(row=3, column=5, sticky= W+E)
-        Button(self.wind, text="=", width= 9, height= 3).grid(row=4, column=4, sticky= W+E, columnspan= 2)
+        Button(self.wind, text=u"\u232B", width= 9, height= 3, command=lambda:Undo()).grid(row=1, column=4, sticky= W+E, columnspan= 2)
+        Button(self.wind, text="EXP", width= 9, height= 3, command=lambda:get_operations("**")).grid(row=2, column=4, sticky= W+E)
+        Button(self.wind, text="^2", width= 9, height= 3, command=lambda:get_operations("**2")).grid(row=2, column=5, sticky= W+E)
+        Button(self.wind, text="(", width= 9, height= 3, command=lambda:get_operations("(")).grid(row=3, column=4, sticky= W+E)
+        Button(self.wind, text=")", width= 9, height= 3, command=lambda:get_operations(")")).grid(row=3, column=5, sticky= W+E)
+        Button(self.wind, text="=", width= 9, height= 3, command=lambda:calculate()).grid(row=4, column=4, sticky= W+E, columnspan= 2)
 
+        #method that takes an index and number
         def get_numbers(n):
             global i 
             display.insert(i, n)
             i+=1
-        
+
+        def get_operations(operator):
+            global i
+            op_lenght = len(operator)   
+            display.insert(i, operator)
+            i+=op_lenght
+
+        def clear():
+            display.delete(0, END)
+
+        def Undo():
+            display_state = display.get()
+            if len(display_state):
+                display_new_state = display_state[:-1]
+                clear()
+                display.insert(0, display_new_state)
+            else:
+                clear()        
+                display.insert(0, 'error')
+
+        def calculate():
+            display_state = display.get()
+            try:
+                math_expr =parser.expr(display_state).compile()
+                result=  eval(math_expr)
+                clear()
+                display.insert(0, result)
+            except:
+                clear()
+                display.insert(0, 'Error')
+
 
         
 
